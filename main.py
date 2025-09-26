e2156fb78292d41c8b904b9fce19fdb7ddb5ba8c
jzpacheco <jeffersonpacheco@hotmail.com>
2025-09-26 15:38:38 -0300

    feat: basic prompt interaction

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,19 +12,32 @@ client = genai.Client(api_key=api_key)
 
 def main():
     args = sys.argv
+    verbose = False
+    
+    for arg in args:
+        if arg == "--verbose":
+            verbose = True
+
     if len(args)<2:
         print('You should inform a prompt when running.')
         sys.exit(1)
 
     prompt = args[1]
+
+    messages =[
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=prompt
+        contents=messages
     )
 
-    meta_data=response.usage_metadata
-    print('Prompt tokens:',meta_data.prompt_token_count)
-    print('Response tokens:',meta_data.candidates_token_count)
+    if verbose:
+        meta_data=response.usage_metadata
+        print('User prompt:',prompt)
+        print('Prompt tokens:',meta_data.prompt_token_count)
+        print('Response tokens:',meta_data.candidates_token_count)
 
 if __name__ == "__main__":
     main()
