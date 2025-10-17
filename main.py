d4dda3130adc41e753a713e964b739df08d3e44e
jzpacheco <jeffersonpacheco@hotmail.com>
2025-10-16 23:46:54 -0300

    feat: created loop to iterate over llm with new messages

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from call_function import available_functions
-from system_prompt import system_prompt
+from call_function import available_functions, call_function
+from config import MAX_AGENT_ITERATIONS
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -31,28 +32,52 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)])
     ]
 
+    for _ in range(MAX_AGENT_ITERATIONS):
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print('Final Response:\n', final_response)
+                break
+            
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
         config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
+    if verbose:
+        meta_data=response.usage_metadata
+        print('Prompt tokens:',meta_data.prompt_token_count)
+        print('Response tokens:',meta_data.candidates_token_count)
 
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
     func_calls = response.function_calls
     if not func_calls:
         return response.text
-    
-    if func_calls:
-        for call in func_calls:
-            print(f"Calling function: {call.name}({call.args})")
-    else:
-        print(response.text)
 
-    if verbose:
-        meta_data=response.usage_metadata
-        print('User prompt:',prompt)
-        print('Prompt tokens:',meta_data.prompt_token_count)
-        print('Response tokens:',meta_data.candidates_token_count)
+    function_responses = []
+    for call in func_calls:
+        function_call_result = call_function(function_call_part=call,verbose=verbose)
+        func_response = function_call_result.parts[0].function_response
 
+        if not func_response or not function_call_result.parts:
+            raise Exception("No response  for function found!!")
+
+        if verbose:
+            print(f"-> {func_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(
+        types.Content(parts=function_responses, role='user')
+    )
 if __name__ == "__main__":
     main()
